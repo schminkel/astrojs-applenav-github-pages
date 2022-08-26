b7eeb13651486b7c771cc0b30d144bce955f0ca6
Thorsten Schminkel <thorsten@schminkel.de>
2022-08-26 19:30:54 +0200

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,5 @@ export default defineConfig(
   {
     site: 'https://schminkel.github.io',
     base: '/astrojs-applenav-github-pages',
-    // Resolves to the "./public" directory, relative to this config file
-    publicDir: new URL("./public", import.meta.url),
   }
 );

3f2a5ec6a86af44750d9b0da5db7a5c7987e7cee
Thorsten Schminkel <thorsten@schminkel.de>
2022-08-26 20:28:59 +0200

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig(
   {
+    integrations: [image()],
     site: 'https://schminkel.github.io',
     base: '/astrojs-applenav-github-pages',
   }
